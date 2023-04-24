4005cd070d11c2397f3d05a5fcda2505de5a40b4
German Fica <me@germanfica.com>
2023-04-24 19:46:11 -0300

    Add ShowErrorNotification

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Toolkit.Uwp.Notifications;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,7 +16,8 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
-            ShowExampleNotification();
+            //ShowExampleNotification();
+            ShowErrorNotification();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
@@ -30,6 +32,21 @@ namespace WindowsFormsApp1
                 .AddText("Andrew sent you a picture")
                 .AddText("Check this out, The Enchantments in Washington!")
                 .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 6 (or later), then your TFM must be net6.0-windows10.0.17763.0 or greater
+        }
+
+        public static void ShowErrorNotification()
+        {
+            string imagePath = Path.Combine(Directory.GetCurrentDirectory(), "Assets", "warning.png");
+
+            new ToastContentBuilder()
+                .AddText("Error")
+                .AddText("Se ha producido un error en la aplicación.")
+                .AddButton(new ToastButton("Ver detalles", "action=details"))
+                .AddButton(new ToastButtonDismiss("Cerrar"))
+                .SetToastDuration(ToastDuration.Long)
+                .AddAppLogoOverride(new Uri(imagePath))
+                .AddAudio(new ToastAudio() { Src = new Uri("ms-winsoundevent:Notification.Looping.Alarm2"), Silent = false })
+                .Show();
         }
     }
 }
